a2b959d6b82311c3eff964ef42e491a7e718d5f4
PedroLucas03 <engs-pedrolubina@camporeal.edu.br>
2025-03-25 01:10:59 -0300

    V3

---
+++ Assets/Scripts/Menu Principal.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    [SerializeField] private string gameNameLevel;
+    [SerializeField] private GameObject MenuInicial;
+    [SerializeField] private GameObject Opcoes;
+    [SerializeField] private GameObject Creditos;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Teste");
+    }
+
+    public void Options()
+    {
+        MenuInicial.SetActive(false);
+        Opcoes.SetActive(true);
+
+    }
+
+    public void CloseOptions()
+    {
+        Opcoes.SetActive(false);
+        MenuInicial.SetActive(true);
+    }
+
+    public void Credits()
+
+    {
+        MenuInicial.SetActive(false);
+        Creditos.SetActive(true);
+      
+    }
+
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+
+    }
+}
